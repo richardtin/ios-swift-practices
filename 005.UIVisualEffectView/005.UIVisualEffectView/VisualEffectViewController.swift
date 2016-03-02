7b37469500f1cf6782eed4cbdaef3b6af7b42a6c
Richard Ting <richard.tin@gmail.com>
2016-03-02 23:05:46 +0800

    Practice 5: UIVisualEffectView
    * Blur effect is easily done by UIVisualEffectView
    * Vibrancy effect is done by one UIVisuallEffectView and nested in another blur UIVisualEffectView
    * This practice is done by StoryBoard ONLY.

---
+++ 005.UIVisualEffectView/005.UIVisualEffectView/VisualEffectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VisualEffectViewController.swift
+//  005.UIVisualEffectView
+//
+//  Created by Richard Ting on 2/29/16.
+//  Copyright © 2016 Richard Ting. All rights reserved.
+//
+
+import UIKit
+
+class VisualEffectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
